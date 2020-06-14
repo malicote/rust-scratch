f8b93058e2f927cb3d89fee0c7f0fdf9218fbca1
Troy B <troy.bevis@gmail.com>
2020-06-14 16:20:12 -0700

    tree: initial commit
    Initial commit for a cli tool to print a directory tree.

---
+++ tree/README.md
@@ -0,0 +1,2 @@
+# Tree
+A small CLI utility that prints a visual representation of a directory and all its subdirectories.
---
+++ tree/src/main.rs
@@ -0,0 +1,11 @@
+use std::io;
+use std::fs::{DirEntry};
+use std::path::Path;
+
+fn visit_dirs(dir: &Path) -> io::Result<()> {
+   Ok(()) 
+}
+
+fn main() {
+    println!("Hello, world!");
+}
