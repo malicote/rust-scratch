1451cc9970444f70deb23c94c8cb25344846374d
Troy B <troy.bevis@gmail.com>
2020-06-20 10:42:10 -0700

    tree: add traversal and basic print
    Add logic to traverse a directory tree and print entires with
    appropriate indentation.

--- tree/src/main.rs
+++ tree/src/main.rs
@@ -1,11 +1,35 @@
 use std::io;
-use std::fs::{DirEntry};
+use std::fs;
 use std::path::Path;
 
-fn visit_dirs(dir: &Path) -> io::Result<()> {
-   Ok(()) 
+fn visit_dirs<F>(dir: &Path, depth: usize, cb: F) -> io::Result<()>
+    where F: Fn(&Path, usize) + Copy
+{
+    for entry in fs::read_dir(dir)? {
+        let path= entry?.path();
+
+        cb(&path, depth);
+        if path.is_dir() {
+            visit_dirs(&path, depth + 1, cb)?;
+        }
+
+    }
+
+    Ok(())
 }
 
-fn main() {
-    println!("Hello, world!");
+fn main() -> io::Result<()> {
+    let root = Path::new(".");
+
+    let result = visit_dirs(root, 0, |path: &Path, depth: usize| {
+        let filename = path.file_name().unwrap().to_string_lossy();
+        println!("{}|-- {}", "|\t".repeat(depth), filename);
+    });
+
+    match result {
+        Err(error) => println!("Error traversing directory {}: {}", root.to_string_lossy(), error),
+        Ok(_) => (),
+    }
+
+    Ok(())
 }
